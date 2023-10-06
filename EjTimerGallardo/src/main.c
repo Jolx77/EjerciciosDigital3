a0eebcfa51d44d4d04a80b5eda0649f2404db4aa
Jolx77 <104650875+Jolx77@users.noreply.github.com>
2023-10-06 13:48:48 -0300

    Update main.c

--- EjTimerGallardo/src/main.c
+++ EjTimerGallardo/src/main.c
@@ -10,21 +10,20 @@
  * activate or otherwise use the software.
  */
 
-#ifdef __USE_CMSIS
-#include "LPC17xx.h"
-#include "lpc17xx_pinsel.h"
-#include "lpc17xx_gpio.h"
-#include "lpc17xx_exti.h"
-#include "lpc17xx_exti.h"
-#endif
 
-#include <cr_section_macros.h>
+#include "LPC17xx.h"
+
+#include "C:\Users\joaqu\Documents\MCUXpressoIDE_11.8.0_1165\workspace4\CMSISv2p00_LPC17xx\Drivers\inc\lpc17xx_pinsel.h"
+#include "C:\Users\joaqu\Documents\MCUXpressoIDE_11.8.0_1165\workspace4\CMSISv2p00_LPC17xx\Drivers\inc\lpc17xx_gpio.h"
+#include "C:\Users\joaqu\Documents\MCUXpressoIDE_11.8.0_1165\workspace4\CMSISv2p00_LPC17xx\Drivers\inc\lpc17xx_exti.h"
+#include "C:\Users\joaqu\Documents\MCUXpressoIDE_11.8.0_1165\workspace4\CMSISv2p00_LPC17xx\Drivers\inc\lpc17xx_timer.h"
+
 
 volatile uint8_t periodo = 1;
 uint8_t estaenT1 = 1;
 uint8_t savePeriodo;
 uint32_t secuencia = 0xA0A3;
-uint8_t PerdonFerPorTantoGlobal = 0; //Sirve para avisar que hay que esperar otros 5 ms x estar en alto
+uint8_t estoyEnAlto = 0; //Sirve para avisar que hay que esperar otros 5 ms x estar en alto
 uint8_t counter = 1; //Empieza en 1 porque la primera recorrida la hacemos en el handler de la interrupcion
 uint8_t polaridad = 0;
 
@@ -39,8 +38,6 @@ void configTimer0(){
 	pinCfg.Funcnum = PINSEL_FUNC_1;
 	PINSEL_ConfigPin(&pinCfg);
 
-	CLKPWR_SetPCLKDiv(CLKPWR_PCLKSEL_TIMER0,CLKPWR_PCLKSEL_CCLK_DIV_2);
-
 	TIM_TIMERCFG_Type timCfg;
 	timCfg.PrescaleOption = TIM_PRESCALE_TICKVAL;
 	timCfg.PrescaleValue = 5000;
@@ -49,19 +46,17 @@ void configTimer0(){
 	matCfg.ExtMatchOutputType = TIM_EXTMATCH_TOGGLE; //Como se en que valor empieza?
 	matCfg.IntOnMatch = ENABLE;
 	matCfg.MatchChannel = 0;
-	matCfg.MatchValue = ((((50*10^-3)/periodo)/(5001))*(50*10^6))-1;
+	matCfg.MatchValue = ((((50*10^-3)/periodo)/(5001))*(100*10^6))-1;
 	matCfg.ResetOnMatch = ENABLE;
 	matCfg.StopOnMatch = DISABLE;
 	savePeriodo = periodo;
 	TIM_Init(LPC_TIM0,TIM_TIMER_MODE,(void*) &timCfg);
-	TIM_ConfigMatch(LPC_TIM0,&mat1Cfg);
+	TIM_ConfigMatch(LPC_TIM0,&matCfg);
 	TIM_Cmd(LPC_TIM0,ENABLE);
-	NVIC_EnableIRQ(TIMER1_IRQn);
+	NVIC_EnableIRQ(TIMER0_IRQn);
 }
 
 void configTimer1(){
-
-	CLKPWR_SetPCLKDiv(CLKPWR_PCLKSEL_TIMER1,CLKPWR_PCLKSEL_CCLK_DIV_2);
 
 	TIM_TIMERCFG_Type tim1Cfg;
 	tim1Cfg.PrescaleOption = TIM_PRESCALE_TICKVAL;
@@ -71,10 +66,10 @@ void configTimer1(){
 	mat1Cfg.ExtMatchOutputType = 0;
 	mat1Cfg.IntOnMatch = ENABLE;
 	mat1Cfg.MatchChannel = 1;
-	mat1Cfg.MatchValue = ((((5*10^-3))/(5001))*(50*10^6))-1;
+	mat1Cfg.MatchValue = ((((5*10^-3))/(5001))*(100*10^6))-1;
 	mat1Cfg.ResetOnMatch = ENABLE;
 	mat1Cfg.StopOnMatch = DISABLE;
-	ConfigStructInit(TIM_TIMER_MODE,(void*) &tim1Cfg);
+	TIM_ConfigStructInit(TIM_TIMER_MODE,(void*) &tim1Cfg);
 	TIM_ConfigMatch(LPC_TIM1,&mat1Cfg);
 	TIM_Cmd(LPC_TIM1,DISABLE);
 }
@@ -99,16 +94,16 @@ void TIMER0_IRQHandler(){
 }
 
 void TIMER1_IRQHandler(){
-	if(PerdonFerPorTantoGlobal == 1){
-		PerdonFerPorTantoGlobal = 0;
+	if(estoyEnAlto == 1){
+		estoyEnAlto = 0;
 		counter++;
 		if(counter == 16){counter = 0;}
 		TIM_ClearIntPending(LPC_TIM0, TIM_CR0_INT);
 		return;
 	}
 
-	if((secuencia & (1<<counter) == 1)){
-		PerdonFerPorTantoGlobal = 1;
+	if((secuencia & (1<<counter)) == 1){
+		estoyEnAlto = 1;
 		LPC_GPIO1->FIOPIN |= (1<<28);
 		TIM_ClearIntPending(LPC_TIM0, TIM_CR0_INT);
 		return;
@@ -116,6 +111,7 @@ void TIMER1_IRQHandler(){
 	else{
 		LPC_GPIO1->FIOPIN &= ~(1<<28);
 		counter++;
+		if(counter == 16){counter = 0;}
 		TIM_ClearIntPending(LPC_TIM0, TIM_CR0_INT);
 		return;
 	}
@@ -138,7 +134,6 @@ void configEINT(void){
 	extiCfg.EXTI_polarity = EXTI_POLARITY_LOW_ACTIVE_OR_FALLING_EDGE;
 	EXTI_Config(&extiCfg);
 	EXTI_ClearEXTIFlag(EXTI_EINT0);
-	NVIC_SetPriority(EINT0_IRQn, 1);
 	NVIC_EnableIRQ(EINT0_IRQn);
 }
 
@@ -169,7 +164,7 @@ void configGPIOINT(){
 	pin03Cfg.Pinmode = PINSEL_PINMODE_NORMAL;
 	PINSEL_ConfigPin(&pin03Cfg);
 
-	LPC_GPIOINT->IO0IntEnF |= 0x0F;
+	LPC_GPIOINT->IO0IntEnF |= 0x0F; //Me interesa que interrumpa tanto por subida como por bajada
 	LPC_GPIOINT->IO0IntEnR |= 0x0F;
 	NVIC_EnableIRQ(EINT3_IRQn);
 
@@ -184,7 +179,7 @@ void EINT3_IRQHandler(){
 void EINT0_IRQHandler(){
 	if(polaridad == 0){
 	polaridad = 1;
-	PerdonFerPorTantoGlobal = 1;
+	estoyEnAlto = 1;
 	counter = 1;
 	PINSEL_CFG_Type pin1Cfg;
 	pin1Cfg.Pinnum = PINSEL_PIN_28;
@@ -192,69 +187,36 @@ void EINT0_IRQHandler(){
 	pin1Cfg.Portnum = PINSEL_PORT_1;
 	pin1Cfg.Funcnum = PINSEL_FUNC_0;
 	PINSEL_ConfigPin(&pin1Cfg);
-	if(secuencia & 1 == 1){
-		PerdonFerPorTantoGlobal = 1;
+	if((secuencia & 1) == 1){
+		estoyEnAlto = 1;
 		LPC_GPIO1->FIOPIN |= (1<<28);
+	}
+	else{
+		LPC_GPIO1->FIOPIN |= ~(1<<28);
 	}
 	TIM_Cmd(LPC_TIM0,DISABLE);
 	TIM_Cmd(LPC_TIM1,ENABLE);
+	NVIC_EnableIRQ(TIMER1_IRQn);
 	EXTI_ClearEXTIFlag(EXTI_EINT0);
-	EXTI_SetPolarity(EINT3_IRQn,EXTI_POLARITY_HIGH_ACTIVE_OR_RISING_EDGE);
+	EXTI_SetPolarity(EINT3_IRQn,EXTI_POLARITY_HIGH_ACTIVE_OR_RISING_EDGE); //Cambiamos la polaridad para leer cuando deje de estar pulsado
 	}
 	else{
 		polaridad = 0;
 		EXTI_SetPolarity(EINT3_IRQn,EXTI_POLARITY_LOW_ACTIVE_OR_FALLING_EDGE);
 		TIM_Cmd(LPC_TIM1,DISABLE);
+		NVIC_DisableIRQ(TIMER1_IRQn);
 		TIM_Cmd(LPC_TIM0,ENABLE);
 		EXTI_ClearEXTIFlag(EXTI_EINT0);
 	}
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 int main(void) {
 
-
-
-
-
-
-
+	configTimer0();
+	configEINT();
+	configTimer1();
+	configGPIOINT();
+	while(1){};
     return 0 ;
 }
